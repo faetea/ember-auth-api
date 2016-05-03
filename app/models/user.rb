4977aa7a90c4f3edd61518be32cfd0d9a7ab34d4
faetea <saphron.scul@gmail.com>
2016-05-02 22:21:10 -0400

    added initializer for paperclip attachment default options (instead of a Hash in each environment), added validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
 
   has_many :collections, dependent: :destroy
 
+  validates :email, :username, uniqueness: true
   # validates :bio, length: { maximum: 500, too_long: "%{count} characters is the maximum allowed" }
 end
